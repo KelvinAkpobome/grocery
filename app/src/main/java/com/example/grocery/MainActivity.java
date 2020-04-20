e691a7e0440e5b5594dd86ae47ce085391fa1687
kelvinakpobome <kelvinakpobome@gmail.com>
2020-04-20 18:10:08 +0100

    added commit

--- app/src/main/java/com/example/grocery/MainActivity.java
+++ app/src/main/java/com/example/grocery/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -13,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutmanger;
 
+    Button share;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         ArrayList<items> exampleList = new ArrayList<>();
         exampleList.add(new items(R.drawable.tomato, "Fresh Tomatoes", "300" ));
@@ -34,5 +41,14 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView.setLayoutManager(mLayoutmanger);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void button(View view) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT,"");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "");
+        shareIntent.setType("text/plain");
+        startActivity(shareIntent);
     }
 }
