311c9e9f34d19485063e460ab6ae9dd11dd26c53
kelvinakpobome <kelvinakpobome@gmail.com>
2020-04-20 15:43:51 +0100

    added commit

--- app/src/main/java/com/example/grocery/MainActivity.java
+++ app/src/main/java/com/example/grocery/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.grocery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutmanger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<items> exampleList = new ArrayList<>();
+        exampleList.add(new items(R.drawable.tomato, "Fresh Tomatoes", "300" ));
+        exampleList.add(new items(R.drawable.cheese, "Cheese", "200" ));
+        exampleList.add(new items(R.drawable.bread, "Bread", "500" ));
+        exampleList.add(new items(R.drawable.milk, "Milk", "450" ));
+        exampleList.add(new items(R.drawable.butter, "Butter", "150" ));
+        exampleList.add(new items(R.drawable.fish, "Fish", "800" ));
+        exampleList.add(new items(R.drawable.meat, "Meat", "1,000" ));
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutmanger = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(exampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutmanger);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
